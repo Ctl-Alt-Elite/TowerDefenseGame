02ab32966dcb08a4f4aa31f989141d6063e50330
GalaxyPotato343 <jodielin0304@gmail.com>
2019-01-14 10:28:38 -0600

    Add files via upload

---
+++ BasicEnemy.java
@@ -0,0 +1,25 @@
+public class BasicEnemy extends Enemy {
+	
+	private final static int HP = 100;
+	private final static int DAMAGEPERHIT = 10;
+	private final static int ATTACKSPEED = 1;
+	private final static int RANGEX = 3;
+	private final static int RANGEY = 3;
+	
+	
+	public BasicEnemy(int positionX, int positionY) {
+		super(HP,DAMAGEPERHIT,ATTACKSPEED,RANGEX,RANGEY,positionX,positionY);
+	}
+
+	@Override
+	public void Attack() {
+		super.getTargetTower().receiveDamage(this.DAMAGEPERHIT);
+		
+	}
+
+	@Override
+	public void Travel() {
+		//TODO will know more when map is more definite. 
+		
+	}
+}
